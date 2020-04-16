66754884deae7a0dbf990e0b3459040c47047685
JunS <jmob0706unpc@live.jp>
2020-04-16 17:47:02 +0900

    [edit] OnHit関数にUFUNCTIONがついていなかったのを修正

--- Source/fps_tutorial/FPSProjectile.h
+++ Source/fps_tutorial/FPSProjectile.h
@@ -37,6 +37,7 @@ public:
 
 	void FireInDirection(const FVector& ShootDirection);
 
+	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NomalImpulse, const FHitResult& Hit);
 
 };
