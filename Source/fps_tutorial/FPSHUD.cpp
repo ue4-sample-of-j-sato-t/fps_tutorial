0dbb54c31b78b2bf19615a3d784b26a430ec3908
JunS <jmob0706unpc@live.jp>
2020-04-16 18:20:40 +0900

    [add] 画面にレティクル表示を追加

---
+++ Source/fps_tutorial/FPSHUD.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSHUD.h"
+#include "Engine/Canvas.h"
+
+void AFPSHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+	// テクスチャがなければ中止
+	if (CrosshairTexture == nullptr) return;
+
+	// キャンバスの中心
+	FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+
+	// テクスチャを中央に表示するための座標
+	FVector2D CrossHairDrawPosition(Center.X - (CrosshairTexture->GetSurfaceWidth() * 0.5f), Center.Y - (CrosshairTexture->GetSurfaceHeight() * 0.5f));
+
+	// 中心にテクスチャを描画
+	FCanvasTileItem TileItem(CrossHairDrawPosition, CrosshairTexture->Resource, FLinearColor::White);
+	TileItem.BlendMode = SE_BLEND_Translucent;
+	Canvas->DrawItem(TileItem);
+}
---
+++ Source/fps_tutorial/FPSHUD.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "FPSHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_TUTORIAL_API AFPSHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+protected:
+
+	// 中央に表示するレティクル
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D* CrosshairTexture;
+
+public:
+
+	// プライマリドローコール
+	virtual void DrawHUD() override;
+};
