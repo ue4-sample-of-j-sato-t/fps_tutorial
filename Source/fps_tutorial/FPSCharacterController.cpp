48b74401cfb8dfe7ffd8e0cea3ee090c9902ced1
JunS <jmob0706unpc@live.jp>
2020-04-20 18:35:35 +0900

    [add] 入力を受け取るコントローラ追加（バインドのみで中身はなし）

---
+++ Source/fps_tutorial/FPSCharacterController.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacterController.h"
+
+AFPSCharacterController::AFPSCharacterController()
+{
+	Super();
+
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AFPSCharacterController::SetupInputComponent()
+{
+	InputComponent->BindAxis("MoveForward", this, &AFPSCharacterController::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &AFPSCharacterController::MoveRight);
+
+	InputComponent->BindAxis("Turn", this, &AFPSCharacterController::TurnCamera);
+	InputComponent->BindAxis("LookUp", this, &AFPSCharacterController::LookupCamera);
+
+	InputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &AFPSCharacterController::StartJump);
+	InputComponent->BindAction("Jump", EInputEvent::IE_Released, this, &AFPSCharacterController::EndJump);
+
+	InputComponent->BindAction("Fire", EInputEvent::IE_Pressed, this, &AFPSCharacterController::FireProjectile);
+}
+
+void AFPSCharacterController::MoveForward(float AxisValue)
+{
+}
+
+void AFPSCharacterController::MoveRight(float AxisValue)
+{
+}
+
+void AFPSCharacterController::TurnCamera(float AxisValue)
+{
+}
+
+void AFPSCharacterController::LookupCamera(float AxisValue)
+{
+}
+
+void AFPSCharacterController::StartJump()
+{
+}
+
+void AFPSCharacterController::EndJump()
+{
+}
+
+void AFPSCharacterController::FireProjectile()
+{
+}
+
+void AFPSCharacterController::Tick(float DeltaTime)
+{
+}
---
+++ Source/fps_tutorial/FPSCharacterController.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FPSCharacterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_TUTORIAL_API AFPSCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AFPSCharacterController();
+	
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+
+	// 入力バインド
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+
+	void TurnCamera(float AxisValue);
+	void LookupCamera(float AxisValue);
+
+	void StartJump();
+	void EndJump();
+
+	void FireProjectile();
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
