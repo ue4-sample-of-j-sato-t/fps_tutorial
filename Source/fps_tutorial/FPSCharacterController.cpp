94cd6ba771a7cdcd14f07215a6e46c30e3f1bd53
JunS <jmob0706unpc@live.jp>
2020-04-21 13:50:21 +0900

    [add] コントローラにインターフェース呼び出しを追加

--- Source/fps_tutorial/FPSCharacterController.cpp
+++ Source/fps_tutorial/FPSCharacterController.cpp
@@ -2,10 +2,14 @@
 
 
 #include "FPSCharacterController.h"
+#include "GameFramework/Pawn.h"
 
 AFPSCharacterController::AFPSCharacterController()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	MoveDirection = FVector(0.f);
+	Rotation = FRotator(0.f);
 }
 
 void AFPSCharacterController::SetupInputComponent()
@@ -22,34 +26,58 @@ void AFPSCharacterController::SetupInputComponent()
 	InputComponent->BindAction("Fire", EInputEvent::IE_Pressed, this, &AFPSCharacterController::FireProjectile);
 }
 
+void AFPSCharacterController::OnPossess(APawn* InPawn)
+{
+	// 一旦クリア
+	CharacterInterface = nullptr;
+
+	if (InPawn->GetClass()->ImplementsInterface(UFPSCharacterInterface::StaticClass()))
+	{
+		CharacterInterface = Cast<IFPSCharacterInterface>(InPawn);
+	}
+}
+
 void AFPSCharacterController::MoveForward(float AxisValue)
 {
+	MoveDirection.X = AxisValue;
 }
 
 void AFPSCharacterController::MoveRight(float AxisValue)
 {
+	MoveDirection.Y = AxisValue;
 }
 
 void AFPSCharacterController::TurnCamera(float AxisValue)
 {
+	Rotation.Pitch = AxisValue;
 }
 
 void AFPSCharacterController::LookupCamera(float AxisValue)
 {
+	Rotation.Yaw = AxisValue;
 }
 
 void AFPSCharacterController::StartJump()
 {
+	if (!CharacterInterface) return;
+	CharacterInterface->StartJumpIF();
 }
 
 void AFPSCharacterController::EndJump()
 {
+	if (!CharacterInterface) return;
+	CharacterInterface->EndJumpIF();
 }
 
 void AFPSCharacterController::FireProjectile()
 {
+	if (!CharacterInterface) return;
+	CharacterInterface->FireIF();
 }
 
 void AFPSCharacterController::Tick(float DeltaTime)
 {
+	if (!CharacterInterface) return;
+	CharacterInterface->MoveIF(MoveDirection);
+	CharacterInterface->RotationIF(Rotation);
 }
--- Source/fps_tutorial/FPSCharacterController.h
+++ Source/fps_tutorial/FPSCharacterController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "FPSCharacterInterface.h"
 #include "FPSCharacterController.generated.h"
 
 /**
@@ -13,12 +14,22 @@ UCLASS()
 class FPS_TUTORIAL_API AFPSCharacterController : public APlayerController
 {
 	GENERATED_BODY()
+private:
+	// 移動量
+	FVector MoveDirection;
+
+	// 回転量
+	FRotator Rotation;
+
+	// インターフェースへのキャッシュ
+	IFPSCharacterInterface* CharacterInterface;
 
 public:
 	AFPSCharacterController();
 	
 protected:
 	virtual void SetupInputComponent() override;
+	virtual void OnPossess(APawn* InPawn) override;
 
 private:
 
