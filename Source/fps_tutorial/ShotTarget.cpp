4e67aefee20109384d93fea259af8dd0c103df63
JunS <jmob0706unpc@live.jp>
2020-04-21 17:21:25 +0900

    [add] 的アクタの見た目作成

---
+++ Source/fps_tutorial/ShotTarget.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShotTarget.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AShotTarget::AShotTarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;	// 当たったときの反応だけのはずなのでTickは切る
+
+	Root = CreateDefaultSubobject<USceneComponent>("Root Component");
+	Root->SetupAttachment(RootComponent);
+
+	TargetMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TargetMesh"));
+	TargetMesh->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void AShotTarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShotTarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/fps_tutorial/ShotTarget.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ShotTarget.generated.h"
+
+UCLASS()
+class FPS_TUTORIAL_API AShotTarget : public AActor
+{
+	GENERATED_BODY()
+
+private:
+
+	// ルート
+	USceneComponent* Root;
+
+	// 表示物
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* TargetMesh;
+	
+public:	
+	// Sets default values for this actor's properties
+	AShotTarget();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
