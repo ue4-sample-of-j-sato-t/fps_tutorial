f225e0932346f1993c53a41455dc8ab655116b2c
JunS <jmob0706unpc@live.jp>
2020-04-22 15:58:39 +0900

    [add] 的を生成するアクタを作成

---
+++ Source/fps_tutorial/TargetFactory.cpp
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TargetFactory.h"
+#include "Components/BoxComponent.h"
+#include "ShotTarget.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Engine.h"
+
+// Sets default values
+ATargetFactory::ATargetFactory()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	RootComponent = SceneComponent;
+
+	SpawnArea = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnArea"));
+	SpawnArea->SetupAttachment(SceneComponent);
+	SpawnArea->BodyInstance.SetCollisionProfileName("NoCollision");
+
+	SpawnMinSec = 1.f;
+	SpawnMaxSec = 2.f;
+}
+
+void ATargetFactory::SetupNextSpawnTime()
+{
+	// パラメータのセーフティ
+	if (SpawnMinSec > SpawnMaxSec)
+	{
+		float Temp = SpawnMinSec;
+		SpawnMinSec = SpawnMaxSec;
+		SpawnMaxSec = Temp;
+	}
+
+	// 次にスポーンする時間をセット
+	NextSpawnSec = FMath::RandRange(SpawnMinSec, SpawnMaxSec);
+}
+
+void ATargetFactory::CheckSpawnTime(float DeltaTime)
+{
+	NextSpawnSec -= DeltaTime;
+
+	if (NextSpawnSec < 0.f)
+	{
+		SpawnTarget();
+		SetupNextSpawnTime();
+	}
+}
+
+void ATargetFactory::SpawnTarget()
+{
+	// ワールド取得
+	UWorld* World = GetWorld();
+	if (!World)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("No world"));
+		return;
+	}
+
+	// 的クラスチェック
+	if (TargetActorClass == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("TargetClass disable"));
+		return;
+	}
+
+	// 生成位置
+	FVector SpawnLocation = UKismetMathLibrary::RandomPointInBoundingBox(SpawnArea->GetRelativeLocation(), SpawnArea->GetScaledBoxExtent());
+	// アクタからの相対座標へ
+	SpawnLocation = GetActorLocation() + (GetActorRotation().RotateVector(SpawnLocation));
+	GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Cyan, SpawnLocation.ToString());
+
+	// 生成
+	FActorSpawnParameters SpawnParam;
+	SpawnParam.Owner = this;
+	SpawnParam.Instigator = Instigator;
+
+	AShotTarget* Target = World->SpawnActor<AShotTarget>(TargetActorClass, SpawnLocation, GetActorRotation(), SpawnParam);
+}
+
+// Called when the game starts or when spawned
+void ATargetFactory::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetupNextSpawnTime();
+}
+
+// Called every frame
+void ATargetFactory::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CheckSpawnTime(DeltaTime);
+}
+
---
+++ Source/fps_tutorial/TargetFactory.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TargetFactory.generated.h"
+
+class AShotTarget;
+class UBoxComponent;
+
+UCLASS()
+class FPS_TUTORIAL_API ATargetFactory : public AActor
+{
+	GENERATED_BODY()
+
+private:
+	// 次にスポーンするまでの時間（秒）
+	float NextSpawnSec;
+
+public:
+	// 配置する的クラス
+	UPROPERTY(EditAnywhere, Category = "Factory")
+	TSubclassOf<class AShotTarget> TargetActorClass;
+
+	// 配置する領域
+	UPROPERTY(EditDefaultsOnly, Category = "Factory")
+	UBoxComponent* SpawnArea;
+
+	// スポーンする最小間隔（秒）
+	UPROPERTY(EditAnywhere, Category = "Factory/SpawnTime")
+	float SpawnMinSec;
+	// スポーンする最大間隔（秒）
+	UPROPERTY(EditAnywhere, Category = "Factory/SpawnTime")
+	float SpawnMaxSec;
+
+public:	
+	// Sets default values for this actor's properties
+	ATargetFactory();
+
+private:
+
+	// 次回のスポーンまでの間隔を決める
+	void SetupNextSpawnTime();
+
+	// 今回スポーンするかを判断する
+	void CheckSpawnTime(float DeltaTime);
+
+	// 領域内に的アクタをスポーンする
+	void SpawnTarget();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
