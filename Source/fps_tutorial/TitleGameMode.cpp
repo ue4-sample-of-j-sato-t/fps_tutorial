83c922b8be12a31d3a866cde40e7693ac0c91606
JunS <jmob0706unpc@live.jp>
2020-04-23 18:05:28 +0900

    [add] タイトル画面の見た目作成

---
+++ Source/fps_tutorial/TitleGameMode.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TitleGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
+ATitleGameMode::ATitleGameMode() : Super()
+{
+	// デフォルトを外す
+	DefaultPawnClass = nullptr;
+}
+
+void ATitleGameMode::MovePlayLevel()
+{
+	UGameplayStatics::OpenLevel(this, PlayLevelPath);
+}
